e63145452f059d1570f1bff00859c83a19cbac83
Gurpreet Sidhu <gsidhu@csumb.edu>
2020-01-28 22:09:22 -0800

    Initial Commit

---
+++ TipCalculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator2
+//
+//  Created by Gurpreet Sidhu on 1/28/20.
+//  Copyright © 2020 gsidhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TipCalculator2Tests/TipCalculator2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TipCalculator2Tests.swift
+//  TipCalculator2Tests
+//
+//  Created by Gurpreet Sidhu on 1/28/20.
+//  Copyright © 2020 gsidhu. All rights reserved.
+//
+
+import XCTest
+@testable import TipCalculator2
+
+class TipCalculator2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
